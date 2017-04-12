18f7ab7197dce0a25fe4c867802b5f824851abaf
Icebob <mereg.norbert@gmail.com>
2017-04-12 16:29:36 +0200

    properties suite

---
+++ suites/properties.js
@@ -0,0 +1,75 @@
+"use strict";
+
+let Promise	= require("bluebird");
+
+let Benchmarkify = require("benchmarkify");
+let benchmark = new Benchmarkify("Object properties").printHeader();
+
+let bench = benchmark.createSuite("Create object with many properties");
+
+// ----
+
+bench.add("Create object with 1 prop", () => {
+	let opts = {
+		prop1: 5
+	};
+	return opts;
+});
+
+bench.add("Create object with 8 prop", () => {
+	let opts = {
+		prop1: 5,
+		prop2: "",
+		prop3: false,
+		prop4: 1,
+		prop5: 0,
+		prop6: null,
+		prop7: "Hello",
+		prop8: 12345
+	};
+	return opts;
+});
+
+bench.add("Create object with 10 prop", () => {
+	let opts = {
+		prop1: 5,
+		prop2: "",
+		prop3: false,
+		prop4: 1,
+		prop5: 0,
+		prop6: null,
+		prop7: "Hello",
+		prop8: 12345,
+		prop9: "asd",
+		prop10: false
+	};
+	return opts;
+});
+
+bench.add("Create object with 20 prop", () => {
+	let opts = {
+		prop1: 5,
+		prop2: "",
+		prop3: false,
+		prop4: 1,
+		prop5: 0,
+		prop6: null,
+		prop7: "Hello",
+		prop8: 12345,
+		prop9: "asd",
+		prop10: false,
+		prop11: 5,
+		prop12: "",
+		prop13: false,
+		prop14: 1,
+		prop15: 0,
+		prop16: null,
+		prop17: "Hello",
+		prop18: 12345,
+		prop19: "asd",
+		prop20: false
+	};
+	return opts;
+});
+
+bench.run();
