c3b4ebcf9ee6cd1e733754ccab2f2bb20b0cf619
Icebob <mereg.norbert@gmail.com>
2020-05-24 11:03:06 +0200

    update promise result

--- suites/promise.js
+++ suites/promise.js
@@ -18,7 +18,7 @@ bench.add("No promise", done => {
 	done();
 }, false);
 
-bench.add("ES6 Promise.resolve", done => {
+bench.ref("ES6 Promise.resolve", done => {
 	return Promise.resolve().then(() => {
 		add(5, 8);
 		done();
@@ -95,7 +95,7 @@ bench.add("await", async done => {
 	done();
 });
 
-bench.add("Aigle Promise.resolve + 5 x then", async done => {
+bench.add("await 5 x", async done => {
 	await add(5, 8);
 	await add(3, 2);
 	await add(9, 3);
@@ -114,35 +114,37 @@ bench.run();
 
 Platform info:
 ==============
-   Windows_NT 6.1.7601 x64
-   Node.JS: 10.16.0
-   V8: 6.8.275.32-node.52
+   Windows_NT 10.0.18363 x64
+   Node.JS: 12.14.1
+   V8: 7.7.299.13-node.16
    Intel(R) Core(TM) i7-4770K CPU @ 3.50GHz × 8
 
 Suite: Promise vs BlueBird vs Aigle vs await
-√ No promise*                               169,827,247 rps
-√ ES6 Promise.resolve*                       11,274,951 rps
-√ ES6 new Promise*                           11,719,615 rps
-√ Bluebird Promise.resolve*                   5,079,560 rps
-√ Bluebird Promise.resolve + 5 x then*        1,105,344 rps
-√ Bluebird new Promise*                       7,109,401 rps
-√ Aigle Promise.resolve*                     12,550,337 rps
-√ Aigle Promise.resolve + 5 x then*           3,226,746 rps
-√ Aigle new Promise*                          8,765,586 rps
-√ await*                                      7,436,999 rps
-√ Aigle Promise.resolve + 5 x then*           2,344,155 rps
+√ No promise*                               136,212,065 rps
+√ ES6 Promise.resolve*                       12,646,841 rps
+√ ES6 new Promise*                           11,579,923 rps
+√ ES6 Promise.resolve + 5 x then*             2,778,475 rps
+√ Bluebird Promise.resolve*                   5,536,574 rps
+√ Bluebird Promise.resolve + 5 x then*        1,101,284 rps
+√ Bluebird new Promise*                       5,133,918 rps
+√ Aigle Promise.resolve*                      9,730,740 rps
+√ Aigle Promise.resolve + 5 x then*           2,545,289 rps
+√ Aigle new Promise*                          5,970,670 rps
+√ await*                                      9,408,408 rps
+√ await 5 x*                                  3,140,225 rps
 
-   No promise*                                    0%    (169,827,247 rps)   (avg: 5ns)
-   ES6 Promise.resolve*                      -93.36%     (11,274,951 rps)   (avg: 88ns)
-   ES6 new Promise*                           -93.1%     (11,719,615 rps)   (avg: 85ns)
-   Bluebird Promise.resolve*                 -97.01%      (5,079,560 rps)   (avg: 196ns)
-   Bluebird Promise.resolve + 5 x then*      -99.35%      (1,105,344 rps)   (avg: 904ns)
-   Bluebird new Promise*                     -95.81%      (7,109,401 rps)   (avg: 140ns)
-   Aigle Promise.resolve*                    -92.61%     (12,550,337 rps)   (avg: 79ns)
-   Aigle Promise.resolve + 5 x then*          -98.1%      (3,226,746 rps)   (avg: 309ns)
-   Aigle new Promise*                        -94.84%      (8,765,586 rps)   (avg: 114ns)
-   await*                                    -95.62%      (7,436,999 rps)   (avg: 134ns)
-   Aigle Promise.resolve + 5 x then*         -98.62%      (2,344,155 rps)   (avg: 426ns)
+   No promise*                              +977.04%    (136,212,065 rps)   (avg: 7ns)
+   ES6 Promise.resolve* (#)                       0%     (12,646,841 rps)   (avg: 79ns)
+   ES6 new Promise*                           -8.44%     (11,579,923 rps)   (avg: 86ns)
+   ES6 Promise.resolve + 5 x then*           -78.03%      (2,778,475 rps)   (avg: 359ns)
+   Bluebird Promise.resolve*                 -56.22%      (5,536,574 rps)   (avg: 180ns)
+   Bluebird Promise.resolve + 5 x then*      -91.29%      (1,101,284 rps)   (avg: 908ns)
+   Bluebird new Promise*                     -59.41%      (5,133,918 rps)   (avg: 194ns)
+   Aigle Promise.resolve*                    -23.06%      (9,730,740 rps)   (avg: 102ns)
+   Aigle Promise.resolve + 5 x then*         -79.87%      (2,545,289 rps)   (avg: 392ns)
+   Aigle new Promise*                        -52.79%      (5,970,670 rps)   (avg: 167ns)
+   await*                                    -25.61%      (9,408,408 rps)   (avg: 106ns)
+   await 5 x*                                -75.17%      (3,140,225 rps)   (avg: 318ns)
 -----------------------------------------------------------------------
 
 */
